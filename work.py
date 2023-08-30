1ddfcd1ecab2c4c3841de79c3b3d3470268a45d1
jaime_esteban_moreno_jaramillo <je.morenoj1@uniandes.edu.co>
2023-08-30 12:28:05 -0500

    avance walk()

--- work.py
+++ work.py
@@ -9,15 +9,21 @@ def upload_txt(txt_direction):
             break
 def revisar_lst(lst):
     #TODO hacer los if
-    estado = True
+    estado = ""
     for i in lst:
         if i == "":
             estado = True
+        elif i == "defVar":
+            estado = True                
+        elif "walk" in i:
+            #falta revisar walk function
+            lst_i = i.split("(").split(")")
+            values = lst_i[1]
         elif estado:
             pass
         else:
             estado = False
-        if estado == False:
+        if not(estado):
             break
     return estado
 upload_txt("a.txt")
