50af607a120f0f5e56fc483202e7813340876600
SCuevas88 <s.cuevas@uniandes.edu.co>
2023-09-01 07:44:00 -0500

    Intento Sergio a partir de linea 33

--- work.py
+++ work.py
@@ -27,3 +27,54 @@ def revisar_lst(lst):
             break
     return estado
 upload_txt("a.txt")
+
+
+
+# Intento Sergio
+defined_names = set()
+
+def upload_txt(txt_direction):
+    with open(txt_direction) as txt:
+        for line in txt:
+            line = line.strip()
+            if not line:
+                continue
+            tokens = line.split()
+            process_tokens(tokens)
+
+def process_tokens(tokens):
+    if tokens[0] == "defVar":
+        define_variable(tokens)
+    elif tokens[0] == "defProc":
+        define_procedure(tokens)
+    elif tokens[0] == "{":
+        pass
+    else:
+        validate_command(tokens)
+
+def define_variable(tokens):
+    if len(tokens) >= 3:
+        variable_name = tokens[1]
+        defined_names.add(variable_name)
+    else:
+        print("Error: Invalid variable definition")
+
+def define_procedure(tokens):
+    if len(tokens) >= 3:
+        procedure_name = tokens[1]
+        defined_names.add(procedure_name)
+    else:
+        print("Error: Invalid procedure definition")
+
+def validate_command(tokens):
+    for token in tokens:
+        if token in defined_names:
+            continue
+        elif token.startswith('(') and token.endswith(')'):
+            function_name = token[1:-1]
+            if function_name not in defined_names:
+                print(f"Error: Undefined function '{function_name}'")
+        else:
+            print(f"Error: Undefined variable or command '{token}'")
+
+upload_txt("a.txt")
